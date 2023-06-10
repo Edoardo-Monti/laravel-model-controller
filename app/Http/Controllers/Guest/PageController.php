1379b6580d65375661e711753ef4bf08f254274b
Your Namede <edoardo.monti.bo@gmail.com>
2023-06-10 15:48:28 +0200

    creazione card con info dal database

--- app/Http/Controllers/Guest/PageController.php
+++ app/Http/Controllers/Guest/PageController.php
@@ -4,15 +4,15 @@ namespace App\Http\Controllers\Guest;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
-use app\Models\Movie;
+use App\Models\Movie;
 
 class PageController extends Controller
 {
     public function index(){
         
-        $Movies = Movie::All();
+        $movies = Movie::All();
 
-        return view('welcome', compact('Movies'));
+        return view('welcome', compact('movies'));
 
     }
 }
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -17,9 +17,16 @@
 
 <body>
 
-    @foreach($Movies as $elem)
+    @foreach($movies as $elem)
 
-    <h2>{{$elem['title']}}</h2>
+    <div class="card" style="width: 18rem;">
+        <div class="card-body">
+            <h5 class="card-title">{{$elem['title']}}</h5>
+            <h6 class="card-subtitle mb-2 text-body-secondary">{{$elem['original_title']}}</h6>
+            <p class="card-link">Data di uscita: {{$elem['date']}}</p>
+            <p class="card-link">Voto: {{$elem['vote']}}</p>
+        </div>
+    </div>
     @endforeach
 </body>
 
