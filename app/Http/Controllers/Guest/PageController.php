7f27c3bfa4241141ada2061f912ccaec8da8f004
Your Namede <edoardo.monti.bo@gmail.com>
2023-06-10 15:30:38 +0200

    prove di collegamento del database

--- app/Http/Controllers/Guest/PageController.php
+++ app/Http/Controllers/Guest/PageController.php
@@ -4,8 +4,15 @@ namespace App\Http\Controllers\Guest;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use app\Models\Movie;
 
 class PageController extends Controller
 {
-    //
+    public function index(){
+        
+        $Movies = Movie::All();
+
+        return view('welcome', compact('Movies'));
+
+    }
 }
